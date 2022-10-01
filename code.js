a8558f8d292d725f8a304786a3b307891b3cbad9
Hrach Mkrtumyan <102722152+hrachm97@users.noreply.github.com>
2022-10-01 23:11:52 +0400

    manually copying code to git

--- code.js
+++ code.js
@@ -2,6 +2,10 @@ const root = document.querySelector("body");
 
 function FileInput(set) {
     const container = document.createElement("input");
+    container.style.width = "200px";
+    container.style.display = "inline-block";
+    container.style.position = "relative";
+    container.style.marginBottom = "10px";
     container.type = "file";
 
     container.onchange = () => {
@@ -14,6 +18,9 @@ function FileInput(set) {
 
 function TypeInput(set) {
     const container = document.createElement("div");
+    container.style.display = "inline-block";
+    container.style.position = "relative";
+    container.style.marginBottom = "10px";
     container.innerHTML = "Type: ";
 
     const input = document.createElement("select");
@@ -37,9 +44,12 @@ function TypeInput(set) {
 
 function AlignInput(set) {
     const container = document.createElement("div");
+    container.style.display = "inline-block";
+    container.style.position = "relative";
+    container.style.marginBottom = "10px";
     
     const center = document.createElement("label");
-    center.innerHTML = "Align: ";
+    center.innerHTML = "Align: </br>";
     
     const input = document.createElement("input");
     input.type = "range";
@@ -80,6 +90,9 @@ function AlignInput(set) {
 
 function TextInput(set) {
     const container = document.createElement("form");
+    container.style.display = "inline-block";
+    container.style.position = "relative";
+    container.style.marginBottom = "10px";
     container.innerHTML = "Text: ";
 
     const input = document.createElement("input");
@@ -92,6 +105,7 @@ function TextInput(set) {
     container.appendChild(button);
     
     container.onsubmit = (e) => {
+        console.log("TypeInput submit");//debug
         e.preventDefault();
         set(input.value);
     }
@@ -101,6 +115,10 @@ function TextInput(set) {
 
 function DurationInput(set) {
     const container = document.createElement("form");
+    //container.style.display = "inline-block";
+    container.style.width = "150px";
+    container.style.position = "relative";
+    container.style.marginBottom = "10px";
     container.innerHTML = "Duration: ";
 
     const input = document.createElement("input");
@@ -122,7 +140,7 @@ function DurationInput(set) {
     return container;
 }
 
-function InputItem() {
+function InputItem(rmv) {
     this.pos = [0,0];
     this.text = ["Type something", 22, "#ffffff"];
     this.typing = false;
@@ -146,8 +164,12 @@ function InputItem() {
 
     const container = document.createElement("form");
     container.innerHTML = "Input Item: ";
+    container.style.display = "inline-block";
+    container.style.position = "relative";
+    container.style.padding = "10px";
 
     const input = document.createElement("input");
+    input.style.display = "block";
     input.type = "text";
     input.value = this.text[0];
 
@@ -177,8 +199,13 @@ function InputItem() {
     container.appendChild(button);
     
     container.onsubmit = (e) => {
+        //debugger;
         e.preventDefault();
-        
+        if(input.value == "") {
+            rmv(this);
+            container.style.display = "none";
+            return;
+        }
         this.setText(input.value, fontSize.value, color.value);
         this.setTyping(typing.checked);
         this.setCursor(cursor.checked);
@@ -191,25 +218,52 @@ function InputItem() {
     return this;
 }
 
-function InputInput(add) {
+function InputInput(add, rmv) {
     const container = document.createElement("div");
-    container.innerHTML = "Inputs: ";
+    container.style.display = "inline-block";
+    container.style.position = "relative";
+    container.style.marginBottom = "10px";
+
+    const header = document.createElement("div");
+    container.style.width = "100px";
+    header.innerHTML = "Inputs: ";
 
     const button = document.createElement("button");
     button.innerHTML = "Add";
 
+    header.appendChild(button);
+
     button.onclick = () => {
-        let newItem = new InputItem();
+        let newItem = new InputItem(rmv);
         add(newItem);
         container.appendChild(newItem.getContainer());
     }
 
-    container.appendChild(button);
+    container.appendChild(header);
 
     return container;
 }
 
-function ShotItem(setImg) {
+function makeSelectable(container){
+    container.style.position = "relative";
+    container.style.outlineWidth = "2px";
+    container.style.outlineColor = "blue";
+    container.style.outlineStyle = "none";
+}
+
+function selectArea() {
+    container = document.createElement("button");
+    container.style.border = "none";
+
+    container.style.position = "absolute";
+    container.style.width = "calc(100% - 20px)";
+    container.style.height = "calc(100% - 20px)";
+    container.style.zIndex = 0;
+
+    return container;
+}
+
+function ShotItem(setImg, rmv) {
     this.duration = 0;
     this.type = 1;
     this.align = 0;
@@ -218,7 +272,7 @@ function ShotItem(setImg) {
         this.duration = _duration;
     }
     this.setType = (_type) => {
-        this.type = _type;
+        this.type = +_type;
     }
     this.setAlign = (_align) => {
         this.align = +_align;
@@ -234,21 +288,57 @@ function ShotItem(setImg) {
         if(this.input === undefined) this.input = [];
         this.input.push(_input);
     }
-
+    this.rmvInput = (_input) => {
+        let index = this.input.indexOf(_input);
+        this.input.splice(index, 1);
+        if(this.input.length === 0) delete this.input;
+    }
+    
     const container = document.createElement("div");
-    container.style.outlineWidth = "0px";
-    container.style.outlineColor = "blue";
-    container.style.outlineStyle = "solid";
-    container.className = "shot";
-    container.style.width = "450px";
+    makeSelectable(container);
+    
+    let selected = false;
+    
+    this.select = () => {
+        if(selected) container.style.outlineStyle = "none";
+        else container.style.outlineStyle = "solid";
+        selected = !selected;
+    }
+    
+    const clickArea = selectArea();
+    container.appendChild(clickArea);
 
-    container.innerHTML = "IMG: ";
+    clickArea.onclick = () => {
+        this.select();
+    }
+
+    const x = document.createElement("button");
+    x.innerHTML = 'X';
+    x.style.position = "absolute";
+    x.style.right = "10px";
+
+    x.onclick = () => {
+        rmv(this);
+        container.style.display = "none";
+    }
+
+    container.appendChild(x);
+
+    container.style.position = "relative";
+    container.style.margin = "10px";
+    container.style.padding = "10px";
+    container.style.backgroundColor = "#eee";
+    container.className = "shot";
+    container.style.width = "280px";
+
     container.appendChild(FileInput(setImg));
     container.appendChild(TypeInput(this.setType));
     container.appendChild(AlignInput(this.setAlign));
     container.appendChild(TextInput(this.setText));
-    container.appendChild(InputInput(this.addInput));
+    container.appendChild(InputInput(this.addInput, this.rmvInput));
     container.appendChild(DurationInput(this.setDuration));
+
+    container.style.display = "inline-block";
 
     this.getContainer = () => {
         return container;
@@ -260,7 +350,7 @@ function ShotItem(setImg) {
 function Audio() {
     const container = document.createElement("audio");
     container.controls = true;
-    container.style.width = "80%";
+    container.style.width = "95%";
     container.style.display = "block";
     const source = document.createElement("source");
 
@@ -278,17 +368,127 @@ function Audio() {
     return this;
 }
 
-function Img() {
-    const container = document.createElement("img");
-    container.style.maxWidth = "400px";
-    container.style.position = "fixed";
-    container.style.right = "0";
-    container.style.top = "0";
+function Display(imgWidth = 400) {
+    const container = document.createElement("div");
+    container.style.minHeight = "200px";
 
-    this.set = (file) => {
-        if(file) container.src = URL.createObjectURL(file);
-        else container.src = "";
+    const display = document.createElement("div");
+
+    const zoom = document.createElement("div");
+    zoom.style.position = "absolute";
+    zoom.style.top = 0;
+
+    const plus = document.createElement("button");
+    const minus = document.createElement("button");
+    plus.style.padding = minus.style.padding = "0px 18px";
+    plus.style.margin = minus.style.margin = "5px";
+    plus.innerHTML = "<h3>+</h3>";
+    minus.innerHTML = "<h3>-</h3>";
+    
+    const img = document.createElement("img");
+    img.style.width = "100%";
+    
+    display.style.width = +imgWidth + "px";
+    display.style.margin = "auto";
+    
+    plus.onclick = () => {
+        imgWidth += 20;
+        display.style.width = imgWidth + "px";
     }
+
+    minus.onclick = () => {
+        imgWidth -= 20;
+        display.style.width = imgWidth + "px";
+    }
+
+    this.setImg = (file) => {
+        if(file) img.src = URL.createObjectURL(file);
+        else img.src = "";
+    }
+    this.getContainer = () => {
+        return container;
+    }
+
+    container.appendChild(display);
+    container.appendChild(zoom);
+    zoom.appendChild(minus);
+    zoom.appendChild(plus);
+    display.appendChild(img);
+
+    return this;
+}
+
+function Shot(setImg, add, rmv, updateDisplay) {
+    const container = document.createElement("div");
+
+    const header = document.createElement("h3");
+    container.style.height = "100px";
+    header.innerHTML = "Shots: ";
+
+    const button = document.createElement("button");
+    button.innerHTML = "<h3>Add</h3>";
+    button.style.padding = "0px 20px";
+    button.style.margin = "0px 10px";
+
+    const shotCount = document.createElement("label");
+    shotCount.style.margin = "0px 10px";
+
+    header.appendChild(button);
+    header.appendChild(shotCount);
+
+    button.onclick = () => {
+        let newItem = new ShotItem(setImg, rmv);
+        updateDisplay(newItem); //adding event
+        shotCount.innerHTML = add(newItem);
+        container.appendChild(newItem.getContainer());
+    }
+
+    container.appendChild(header);
+
+    return container;
+}
+
+function Timeline(setImg, data) {
+    this.shots = [];
+    data.shot = this.shots;
+
+    this.addShot = (_shot) => {
+        this.shots.push(_shot);
+        return this.shots.length;
+    }
+    this.rmvShot = (_shot) => {
+        let index = this.shots.indexOf(_shot);
+        this.shots.splice(index, 1);
+        return this.shots.length;
+    }
+
+    const container = document.createElement("div");
+    container.className = "timeline";
+
+    const JSONconatiner = document.createElement("div");
+    JSONconatiner.style.margin = "10px";
+    JSONconatiner.style.position = "absolute";
+    JSONconatiner.style.left = "200px";
+    JSONconatiner.style.maxHeight = "80px";
+    JSONconatiner.style.overflowY = "scroll";
+    JSONconatiner.style.backgroundColor = "rgb(255,255,255,0.8)";
+
+    function updateDisplay(shotItem) {
+        shotItem.getContainer().onclick = 
+        shotItem.getContainer().onchange = 
+        shotItem.getContainer().onsubmit = () => {
+            JSONconatiner.innerHTML = JSON.stringify(data);
+        }
+    }
+
+    const audio = new Audio();
+
+    container.appendChild(FileInput(audio.set));
+    container.appendChild(audio.getContainer());
+    container.appendChild(JSONconatiner);
+    container.appendChild(Shot(setImg, this.addShot, this.rmvShot, updateDisplay));
+    container.style.marginBottom = "20px";
+
     this.getContainer = () => {
         return container;
     }
@@ -296,37 +496,19 @@ function Img() {
     return this;
 }
 
-function Timeline(shotitem) {
-    const container = document.createElement("div");
-    container.className = "timeline";
-
-    const button = document.createElement("button");
-    button.innerHTML = "Get JSON";
-    button.onclick = () => {
-        alert(JSON.stringify(shotitem));
+function App() {
+    let data = {
+        shot:[],
+        mockup: 1
     }
 
-    const audio = new Audio();
+    const display = new Display();
+    const timeline = new Timeline(display.setImg, data);
+    
+    data.shot = timeline.shots;
 
-    container.appendChild(FileInput(audio.set));
-    container.appendChild(audio.getContainer());
-    container.appendChild(button);
-    container.style.marginBottom = "20px";
-
-    return container;
-}
-
-function App() {
-    const img = new Img();
-
-    let shotitem = new ShotItem(img.set);
-
-    root.appendChild(Timeline(shotitem));
-    root.appendChild(shotitem.getContainer());
-    root.appendChild(img.getContainer());
-    // setInterval(() => {
-    //     alert(JSON.stringify(shotitem))
-    // }, 5000);
+    root.appendChild(display.getContainer());
+    root.appendChild(timeline.getContainer());
 }
 
 App() 
